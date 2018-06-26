6a0f49d9673ea3dc71d2d4aea4eaf982a77d0f66
kush <kushkumardhawan@gmail.com>
2018-06-26 11:51:47 +0530

    Learning Basics

--- Learning/app.js
+++ Learning/app.js
@@ -1,2 +1,28 @@
-alert("Hello World Via Visual Studio Code");
+alert("Javascript Loaded");
 console.log("Hello via Console");
+
+//Variables and Data Types
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 26;
+
+//Number , String, boolean, Undifiened and Null
+//Javascript has dynamic typing i.e. Data types are automatically assigned to variables
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;  //declare and not assign a value to it
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+//Variable name cannot be staarted with a number but $ and underscore_ works perfect
+
+/**
+ * Variable Mutation and Type Cohresion
+ */
+
+
